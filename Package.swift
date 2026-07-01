632248a7feba012b84a0c3c34af5b4ae9a49786b
lynx-ci <ci@synetic.ai>
2026-07-01 13:32:18 -0700

    Swift: Package.swift -> v1.0.1157 xcframework

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LYNX",
-            url: "https://github.com/Syneticai/lynx/releases/download/v0.0.0-placeholder/LYNX.xcframework.zip",
-            checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+            url: "https://github.com/Syneticai/lynx/releases/download/v1.0.1157/LYNX.xcframework.zip",
+            checksum: "5541a426a4cb3a8802511f4821528749fabf01ee23fd5500f039f1844d4b88e9"
         )
     ]
 )
